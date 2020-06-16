6d24f24620cccae4d945f7822141966ab3dccc4b
Neal-Cyber2020 <65833485+Neal-Cyber2020@users.noreply.github.com>
2020-06-15 21:28:04 -0400

    Small Additions and Details

--- main.py
+++ main.py
@@ -42,14 +42,14 @@ if Start == 1:
     print(" |             |             |             |             |             |             |             |             |")
     print(" |_____________|_____________|_____________|_____________|_____________|_____________|_____________|_____________|")
     print("       a              b             c             d             e             f             g             h       ")
-    class Board(object):                                                                #Board Code
+    class Board(object):                                                                    #Board Code
         def __init__(self,nb,ch,ma,vm,ic):
     	    self.New_Board = nb
             self.Check = ch
             self.Mate = ma
-            self.Valid_Move = vm
+            self.Valid_Move = vm                    #Special Effects Could Be Added Here (Invaled Move)(Sound, Appereance, etc...)
             self.Is_Checked = ic
-    class Pieces:                                                                       #Pieces General Code
+    class Pieces:                                                                           #Pieces General Code
         def __init__ (self,ca,ta,it,ia,bl,mo):
             self.captured = ca
             self.take = ta
@@ -57,7 +57,7 @@ if Start == 1:
             self.Is_Available = ia
             self.block = bl
             self.move = mo
-            if ma == True:
+            if ma == True:                          #Special Effects Could Be Added Here (Sound, Appereance, etc...)
                 ia = False
                 it = False
                 bl = False
@@ -65,64 +65,82 @@ if Start == 1:
                 ca = False
                 mo = False
                 print("Game Over")
-            if ch == True:
+            if ch == True:                          #Special Effects Could Be Added Here (Sound, Appereance, etc...)
                 if bl == True:
                     ia = True
                 if ta == True:
                     ia = True
-        class Rook(object):                                                             #Rook Code
+            if nb == True:                          #Special Effects Could Be Added Here (Sound, Appereance, etc...)
+                ta = False
+                bl = False
+                ca = False
+                ia = True
+                mo = True
+        class Rook(object):                                                                 #Rook Code
                 def __init__ (self,c,x,y):
                     self.color = c
                     self.alive = True
                     self.firstMove = True
                     self.x = x
                     self.y = y
+                    if ca == True:
+                        self.alive = False
                def move(self, dirx, diry):
                     if validMove(dirx,diry):
                         self.x= dirx
                         self.y= diry
-        class Knight(object):                                                          #Knight Code
+        class Knight(object):                                                             #Knight Code
                 def __init__ (self,c,x,y):
                     self.color = c
                     self.alive = True
                     self.x = x
                     self.y = y
+                    if ca == True:
+                        self.alive = False
                def move(self, dirx, diry):
                     if validMove(dirx,diry):
                         self.x= dirx
                         self.y= diry
-        class Pawn(object):                                                             #Pawn Code
+        class Pawn(object):                                                               #Pawn Code
                 def __init__ (self,c,x,y):
                     self.color = c
                     self.alive = True
                     self.firstMove = True
+                    if self.firstMove == True:
+                        self.enpassant = True
                     self.x = x
                     self.y = y
+                    if ca == True:
+                        self.alive = False
                def move(self, dirx, diry):
                     if validMove(dirx,diry):
                         self.x= dirx
                         self.y= diry
-        class Bishop(object):                                                           #Bishop Code
+        class Bishop(object):                                                             #Bishop Code
                 def __init__ (self,c,x,y):
                     self.color = c
                     self.alive = True
                     self.x = x
                     self.y = y
+                    if ca == True:
+                        self.alive = False
                def move(self, dirx, diry):
                     if validMove(dirx,diry):
                         self.x= dirx
                         self.y= diry
-        class Queen(object):                                                            #Queen Code
+        class Queen(object):                                                                #Queen Code
                 def __init__ (self,c,x,y):
                     self.color = c
                     self.alive = True
                     self.x = x
                     self.y = y
+                    if ca == True:
+                        self.alive = False
                def move(self, dirx, diry):
                     if validMove(dirx,diry):
                         self.x= dirx
                         self.y= diry
-        class King(object):                                                             #King Code
+        class King(object):                                                                #King Code
                 def __init__ (self,c,x,y):
                     self.color = c
                     self.alive = True
